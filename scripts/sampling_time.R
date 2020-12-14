795ac39fea35870ccb812ede164fa9678850e7c6
Brian Evans <bsevans.unc@gmail.com>
2020-12-14 12:12:08 -0500

    initial commit

---
+++ scripts/sampling_time.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+
+# Get sites:
+
+catSites <-
+  read_csv('data/siteCovs.csv') %>%
+  arrange(site) %>%
+  select(site)
+
+# Generate a list based on the number of months:
+
+samplingList <-
+  purrr::map(
+    # Number of months:
+    1:3,
+    function(x){
+    # Cat sites with month number:
+    catSites %>%
+      mutate(month = x) %>%
+      # Random site generation:
+      sample_frac() %>%
+      mutate(
+        sampleOrder = row_number())
+  })
+
+# Resample months 2 and 3 such that sites are not visited within 14
+# sampling periods:
+
+for(i in 2:length(samplingList)){
+  samplingList[[i]] <- 
+    samplingList[[i-1]] %>%
+    group_by(sampleOrder >= max(sampleOrder) - 14) %>%
+    sample_frac() %>%
+    ungroup %>%
+    mutate(sampleOrder =  row_number()) %>%
+    select(site:sampleOrder)}
+
+bind_rows(samplingList)
+
+
